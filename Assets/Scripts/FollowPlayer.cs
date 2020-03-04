4e5f8a3a8b14d52162c993471ff74542a29950b9
Colthan <avecolth@gmail.com>
2020-03-04 11:42:30 -0800

    first upload

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 Offset = new Vector3(7, 3, 0);
+   
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position + Offset;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Vspeed = 20;
+    public float Hspeed = 10;
+    public float jumpforce = 5;
+    public CapsuleCollider col;
+    public LayerMask groundLayers;
+    private Rigidbody rb;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float VerticalInput = Input.GetAxis("Vertical");
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * Vspeed * VerticalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * Hspeed * HorizontalInput);
+        //transform.Rotate(Vector3.up * Time.deltaTime * Hspeed * HorizontalInput);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+        }
+    }
+    /*private bool IsGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x,
+            col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
+        // SITE: https://www.youtube.com/watch?v=vdOFUFMiPDU 
+    }
+    */
+}
